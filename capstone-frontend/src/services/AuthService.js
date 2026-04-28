7a3168c6f49a63c05011f40d7e7ee75e6ceca8b0
Your Name <you@example.com>
2026-04-28 12:35:07 -0600

    changing AuthService back to signup to fix route error

--- capstone-frontend/src/services/AuthService.js
+++ capstone-frontend/src/services/AuthService.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:3500/api/';
 
+
 export async function signup(name, email, password) {
-    const { data } = await axios.post(`${baseUrl}auth/register`, { name, email, password });
+    const { data } = await axios.post(`${baseUrl}auth/signup`, { name, email, password });
     return data;
 }
 
